e3da72a0ade104d1ab2121a33dc0843af038b29d
Andrew <forkk@forkk.net>
2013-11-16 17:28:55 -0600

    Refactored ALL the things

--- main.py
+++ main.py
@@ -36,6 +36,8 @@ The title of the game. This should remain constant.
 TITLE = "Defensive Design"
 
 
+# TODO: Put this stuff into a settings system.
+
 # Keys for scrolling the screen.
 # Dict for mapping keypresses to the direction they should move the screen.
 # We should add some more extensible system for this later, but this will do for now.
@@ -74,162 +76,172 @@ ZOOM_SPEED = 0.05
 ZOOM_MIN = 0.2
 ZOOM_MAX = 1.5
 
-"""
-This performs initial setup of the game. Any global variables
-should also be defined here (yes, I know most people say global
-variables are bad, but there really isn't a simple solution).
-"""
-def setup():
-   
-    # Set the title of the game.
-    pygame.display.set_caption(TITLE)
-    # Set up a new window.
-    global ScreenSurface
-    ScreenSurface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+class Game(object):
+    """
+    The main game object.
+    Holds all the stuff.
+    """
+    def __init__(self, map_name):
+        """
+        Initializes a new game object with the given map name.
+        """
+        # Set the title of the game.
+        pygame.display.set_caption(TITLE)
 
-	# We'll have 3 surfaces, the UI surface, the game surface, and the screen surface.
-    # The game surface will be for drawing the ingame stuff to. It'll be translated based on screen view position.
-    # Will be initialized later based on the size of the gamemap.
-    global GameSurface
-
-    global UISurface
-    UISurface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), flags=pygame.SRCALPHA);
-
-
-    # Set up the map
-    global Map
-
-    # Set up the starting game data
-    global Data
-    Data = gamedata.GameData()
-    
-    # Set up the UI
-    global UI
-    UI = userinterface.UserInterface()
-    Map = gamemap.GameMap("map1")
-    gameSize = Map.getMapSize()
-
-    # Now, create the game surface based on the map size.
-    GameSurface = pygame.Surface(Map.getMapSize());
-
-    # Initialize the enemy manager
-    global EnemyManager
-    EnemyManager = enemymanager.EnemyManager(Map.getTileSize())
-
-    global GameClock
-    GameClock = pygame.time.Clock()
-
-    global GameState
-    GameState = True
-
-    global GameCamera
-    GameCamera = camera.Camera(gameSize, (SCREEN_WIDTH, SCREEN_HEIGHT))
-
-"""
-This handles a single pygame event.
-"""
-def handleEvent(event):
-    if(event.type == pygame.KEYDOWN or event.type == pygame.KEYUP):
-        handleKeyEvent(event)
-    if event.type == pygame.QUIT:
-        # Quit the program safely
-        pygame.quit()
-        sys.exit()
-    else:
-        EnemyManager.spawnEnemy(event, Map.getStartingTile())
-
-"""
-This is the main game loop, called as many times as
-the computer allows.
-"""
-def main():
-    while(1):
-        #Handle the event queue
-        event = pygame.event.poll()
-        # The event queue returns an event of type NOEVENT if the queue is empty
-        while(event.type != pygame.NOEVENT):
-            handleEvent(event)
-            event = pygame.event.poll()
-        # Delete anything already on the surface.
-        ScreenSurface.fill((0, 0, 0))
-
-        # If a scroll key is pressed, move the screen.
+        # Set up a new window.
+        self.screen_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         
-        update() # Update the game objects
-        draw() # Draw all the game objects
-        pygame.display.flip()
+        # Set up the UI.
+        self.ui = userinterface.UserInterface()
 
-        # Maintain the max frame rate
-        GameClock.tick(MAX_FPS)
-       
+        # Load the map.
+        self.map = gamemap.GameMap(map_name)
 
-"""
-Handles any updating of game objects. This is called
-once per game loop.
-"""
-def update():
-    global GameState
-    if(GameState):
-        # Update the scren scrolling.
-        global GameCamera
+        # Initialize the enemy manager
+        self.enemy_mgr = enemymanager.EnemyManager(self.map.getTileSize())
+
+        # Initialize the game clock
+        self.clock = pygame.time.Clock()
+
+        self.camera = camera.Camera(self.map.getMapSize(), (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+        # We'll have 2 surfaces, the UI surface, the game surface.
+        # The game surface will be for drawing the ingame stuff. It'll be translated based on camera position.
+        # The UI surface will be for drawing the UI.
+        self.game_surface = pygame.Surface(self.map.getMapSize());
+        self.ui_surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), flags=pygame.SRCALPHA);
+
+        # If False, the main loop will stop running.
+        self.running = False
+
+        # If True, the game logic tick will stop and things will pause.
+        # NOT CURRENTLY IMPLEMENTED. BREAKS ALL THE THINGS
+        self.paused = False
+
+        self.data = gamedata.GameData()
+
+
+    def main(self):
+        """
+        Starts the main game loop.
+        """
+        running = True
+        while (running):
+            # Handle the event queue
+            event = pygame.event.poll()
+
+            # The event queue returns an event of type NOEVENT if the queue is empty
+            while event.type != pygame.NOEVENT:
+                self.handleEvent(event)
+                event = pygame.event.poll()
+
+            # Delete anything already on the surface.
+            self.screen_surface.fill((0, 0, 0))
+
+            # Logic tick stuff.
+            self.tick()
+
+            # Draw the things.
+            self.draw()
+            
+            # Flip display stuff.
+            pygame.display.flip()
+
+            # Maintain dat framerate.
+            self.clock.tick(MAX_FPS)
+
+    def quit(self):
+        """
+        Sets self.running to false, ending the main loop and closing the game.
+        Does not quit pygame or anything.
+        """
+        self.running = False
+
+
+    def tick(self):
+        """
+        Performs a single logic tick.
+        This means updating the camera position, UI status, 
+        and, if the game isn't paused, the game logic.
+        """
+        # Update the enemies
+        self.gameTick()
+
+        # Update the UI.
+        self.uiTick()
+
+    def uiTick(self):
+        """
+        Performs a UI tick.
+        This updates the user interface elements and the camera.
+        """
+        # First, update the camera.
         for key, direction in KEYPRESS_SCROLL_MAP.iteritems():
             if pygame.key.get_pressed()[key]:
-                GameCamera.move((direction[0] * SCROLL_SPEED, direction[1] * SCROLL_SPEED))
+                self.camera.move((direction[0] * SCROLL_SPEED, direction[1] * SCROLL_SPEED))
         for key, zoomfactor in KEYS_ZOOM_VIEW.iteritems():
             if pygame.key.get_pressed()[key]:
-                GameCamera.zoom(zoomfactor * ZOOM_SPEED * (GameCamera.getZoom()))
+                self.camera.zoom(zoomfactor * ZOOM_SPEED * (self.camera.getZoom()))
+        self.camera.tickUpdate()
 
-        GameCamera.tickUpdate()
-        
-        # Update the enemies
-        livesLost = EnemyManager.update(Map)
-        Data.lives -= livesLost
-        
+        # TODO: Update other UI elements.
         # Update the UI
-        UI.update(Data)
+        self.ui.update(self.data)
 
-        # Check if the game is over
-        if(Data.lives <= 0):
-            GameState = False # The game is over
-            UI.showDefeat()
-       
-
-"""
-Draws all game objects to the screen. This is called once
-per game loop.
-"""
-def draw():
-    # Draw the map
-    Map.draw(GameSurface)
-
-    # Draw the enemies
-    EnemyManager.draw(GameSurface)
-
-    # Clear the UI surface to transparent and then draw the UI
-    UISurface.fill(pygame.Color(0, 0, 0, 0))
-    UI.draw(UISurface)
-
-    # Now, we draw the game and UI surfaces onto the screen.
-    ScreenSurface.blit(pygame.transform.scale(GameSurface, GameCamera.getSurfaceSize()), GameCamera.getSurfacePos())
-
-    ScreenSurface.blit(UISurface, (0, 0))
+    def gameTick(self):
+        """
+        Performs a game logic tick.
+        This updates enemies, towers, and other game logic stuff.
+        """
+        # TODO: Make this not stupid.
+        livesLost = self.enemy_mgr.update(self.map)
+        self.data.lives -= livesLost
 
 
-"""
-Handles a single keyboard event (both key down and key up).
-The event passed in is assumed to be a key event, or else
-nothing happens.
-"""
-def handleKeyEvent(event):
-    if(event.type == pygame.KEYDOWN):
-        # If the escape key has been pressed, quit the game safely
-        if(event.key == pygame.K_ESCAPE):
-            pygame.quit()
-            sys.exit()
-    else:
-        if(event.type == pygame.KEYUP):
-            return # TODO: Add stuff for key up events here
+    def draw(self):
+        """
+        Draws the game and UI.
+        """
+        # Draw the map
+        self.map.draw(self.game_surface)
+
+        # Draw the enemies
+        self.enemy_mgr.draw(self.game_surface)
+
+        # Clear the UI surface to transparent and then draw the UI
+        self.ui_surface.fill(pygame.Color(0, 0, 0, 0))
+        self.ui.draw(self.ui_surface)
+
+        # Now, we draw the game and UI surfaces onto the screen.
+        self.screen_surface.blit(pygame.transform.scale(self.game_surface, self.camera.getSurfaceSize()), self.camera.getSurfacePos())
+
+        self.screen_surface.blit(self.ui_surface, (0, 0))
+
+
+    def handleEvent(self, event):
+        if(event.type == pygame.KEYDOWN or event.type == pygame.KEYUP):
+            self.handleKeyEvent(event)
+        if event.type == pygame.QUIT:
+            # Quit the program safely
+            self.quit()
+        else:
+            self.enemy_mgr.spawnEnemy(event, self.map.getStartingTile())
+        
+    def handleKeyEvent(self, event):
+        if(event.type == pygame.KEYDOWN):
+            # If the escape key has been pressed, quit the game safely
+            if(event.key == pygame.K_ESCAPE):
+                # Pause on escape.
+                self.paused = not self.paused
+        else:
+            if(event.type == pygame.KEYUP):
+                return # TODO: Add stuff for key up events here
+
 
 pygame.init()
-setup()
-main()
+# One global game object.
+global MainGame
+MainGame = Game("map1")
+MainGame.main()
+pygame.quit()
+
